ec3e155087a97bd2b55f850ee911fce29f05f51c
fumiya <fumiya2324@gmail.com>
2021-02-04 21:38:42 +0900

    init_commit

---
+++ api.php
@@ -0,0 +1,209 @@
+<?php
+
+// 送られてきたデータのエスケープ処理を行う
+function h($str) {
+
+    $str = htmlspecialchars($str , ENT_QUOTES, 'UTF-8');
+    return $str;
+}
+
+// データベースへ接続を行う
+function connectDB(string $dsn, string $user, string $password) {
+
+    $dbh = new PDO($dsn,$user,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    return $dbh;
+}
+
+// 現在利用している利用者のデータを取ってくる
+function use_now() {
+
+    try {
+        $dbh = connectDB('mysql:dbname=monitoring;host=localhost;charset=utf8', 'root', 'Fumiya_0324');
+        $sql = 'SELECT user.name, log.enter_time '.
+                'FROM log '.
+                'INNER JOIN user ON log.user_id=user.id '.
+                'WHERE log.exit_time IS NULL '.
+                'AND log.enter_time >= DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 1 DAY) '.
+                'ORDER BY log.enter_time DESC';
+        $stmt = $dbh->query($sql);
+        $ret = $stmt->fetchAll();
+        
+        return $ret;
+    }catch(Exception $e) {
+        return $e;
+    }
+}
+
+// 任意のidmのユーザがuserテーブルにいるかどうか確認する
+function check_user($dbh, string $idm) {
+   
+    $sql = 'SELECT * FROM user WHERE idm=?';
+    $stmt = $dbh->prepare($sql);
+    $data[] = $idm;
+    $stmt->execute($data);
+
+    $ret = $stmt->fetch();
+    if ($ret) {
+        return $ret['id'];
+    } else {
+        return false;
+    }
+}
+
+// userテーブルにユーザを登録する
+function add_user($dbh, string $idm, string $name) {
+
+    $sql = 'INSERT INTO user (idm, name) VALUES (?, ?)';
+    $stmt = $dbh ->prepare($sql);
+    $data[] = $idm;
+    $data[] = $name;
+    $stmt->execute($data);
+    
+    return true;
+    
+}
+
+// logテーブルのログを更新する
+function update_log($dbh, string $idm, string $name) {
+
+    if (check_user($dbh, $idm) == false) {
+        add_user($dbh, $idm, $name);
+    }
+
+    $sql = 'SELECT * '.
+            'FROM log INNER JOIN user '.
+            'ON log.user_id=user.id '.
+            'WHERE exit_time IS NULL '.
+            'AND enter_time >= DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 1 DAY) '.
+            'AND user.idm=? '.
+            'ORDER BY log.enter_time DESC LIMIT 1';
+
+    $stmt = $dbh->prepare($sql);
+    $data[] = $idm;
+    $stmt->execute($data);
+    unset($data);
+    $ret = $stmt->fetch();
+
+    if ($ret) {
+        // 退出する
+
+        $sql = 'UPDATE log '.
+                'SET exit_time=CURRENT_TIMESTAMP '.
+                'WHERE exit_time IS NULL '.
+                'AND user_id=? '.
+                'ORDER BY enter_time DESC LIMIT 1';
+        $stmt = $dbh->prepare($sql);
+        $data[] = $ret['user_id'];
+        $stmt->execute($data);
+        unset($data);
+
+        $retarr = [
+            'result' => 'out'
+        ];
+        return $retarr;
+    } else {
+        // 新しく入室
+
+        $user_id = check_user($dbh, $idm);
+
+        $sql = 'INSERT INTO log (user_id) VALUES (?)';
+        $stmt = $dbh->prepare($sql);
+        $data[] = $user_id;
+        $stmt->execute($data);
+
+        $retarr = [
+            'result' => 'in'
+        ];
+        return $retarr;
+    }
+}
+
+// 任意の検索条件に当てはまるログデータをとってくる
+function search_log(string $idm, string $name, string $from, string $to) {
+    $sql = 'SELECT * FROM log INNER JOIN user ON log.user_id=user.id WHERE ';
+
+    if ($idm != '') {
+        $sql = $sql.'idm=? ';
+        $data[] = $idm;
+    } else if ($name != '') {
+        $sql = $sql.'name=? ';
+        $data[] = $name;
+    }
+
+    if (($idm != '' || $name != '') && ($from != '' && $to != '')) {
+        $sql = $sql.'AND enter_time BETWEEN ? AND ? OR exit_time BETWEEN ? AND ?';
+        $data[] = $from;
+        $data[] = $to;
+        $data[] = $from;
+        $data[] = $to;
+    } else if ($from != '' && $to != '') {
+        $sql = $sql.'enter_time BETWEEN ? AND ? OR exit_time BETWEEN ? AND ?';
+        $data[] = $from;
+        $data[] = $to;
+        $data[] = $from;
+        $data[] = $to;
+    }
+
+    try {
+        $dbh = connectDB('mysql:dbname=monitoring;host=localhost;charset=utf8', 'root', 'Fumiya_0324');
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
+
+        $retarr = [
+            'result' => 'success',
+            'log' => $stmt->fetchAll(),
+            'from' => $from
+        ];
+        return $retarr;
+    } catch (Exception $e) {
+        $retarr = [
+            'error' => $e
+        ];
+        return $retarr;
+    }
+}
+
+// 送られてくるデータによって処理を分岐する
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $arr = json_decode(file_get_contents('php://input'), true);
+
+    $idm = $arr['idm'];
+    $name = $arr['name'];
+
+    if ($name == false) {
+        $name = 'guest';
+    }
+    
+    try {
+        $dbh = connectDB('mysql:dbname=monitoring;host=localhost;charset=utf8', 'root', 'Fumiya_0324');
+        $dbh->beginTransaction();
+        
+        $retarr = update_log($dbh, $idm, $name);
+        
+        $dbh->commit();
+    } catch (Exception $e) {
+        $dbh->rollBack();
+    } 
+    $dbh = null;
+
+    echo json_encode($retarr);
+
+} else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    header('Content-Type: application/json; charset=UTF-8');
+
+    if ($_GET['command'] == 'search') {
+
+        $from = h($_GET['from']);
+        $to = h($_GET['to']);
+        $idm = h($_GET['idm']);
+        $name = h($_GET['name']);
+
+        echo json_encode(search_log($idm, $name, $from, $to));
+
+    } else if ($_GET['command'] == 'use_now') {
+        echo json_encode(use_now());
+    }
+}
+
+?>
---
+++ index.js
@@ -0,0 +1,100 @@
+jQuery(function($) {
+
+    // 検索ボタンを押したときの処理
+    $('button').on('click.button', function() {
+        let from = $('input[name=from]');
+        let to = $('input[name=to]');
+        let search_condition = $('input[name=search_condition]');
+        let name = '';
+        let idm = '';
+
+        if ((from.val() != '' && to.val() != '') || search_condition.val() != '') {
+
+            if (search_condition != '') {
+                if (search_condition.val().match('[0-9]{2}[A-Z][0-9]{3}')) {
+                    idm = search_condition.val();
+                } else {
+                    name = search_condition.val();
+                }
+            }
+
+            if (from.val() != '' && to.val() != '') {
+                from = from.val();
+                to = to.val();
+            } else {
+                from = '';
+                to = '';
+            }
+
+            console.log(idm, name, from, to);
+
+            $.ajax({
+                url : 'api.php',
+                type : 'GET',
+                dataType : 'json',
+                data : {
+                    command : 'search',
+                    idm : idm,
+                    name : name,
+                    from : from,
+                    to : to
+                }
+            }).done(function(data) {
+
+                let ul = $('ul');
+                
+                search_condition.val('');
+
+                ul.html('');
+
+                if (data.log) {
+                    data.log.forEach(function(element) {
+                        let li = $('<li></li>');
+                        let time = $('<div></div>').addClass('time').text(element.enter_time+'~'+element.exit_time);
+                        let name = $('<div></div>').addClass('name').text(element.name);
+    
+                        li.append(time).append(name);
+                        ul.append(li);
+                    });
+
+                } else {
+                    let li = $('<li></li>').text('検索に一致するログが見つかりませんでした');
+                    ul.apend(li);
+                }
+
+            }).fail(function(data) {
+                alert('通信に失敗しました');
+            })
+
+        } else {
+            alert('入力値が正しくありません');
+        }
+    });
+
+    // 一定時間ごとに現在入室している人のデータを取ってくる
+    $(function (){
+        setInterval(function (){
+            $.ajax({
+                url : 'api.php',
+                type : 'GET',
+                dataType : 'json',
+                data : {
+                    command : 'use_now'
+                }
+            }).done(function(data) {
+                let use_now = $('.use_now');
+                use_now.html('');
+                data.forEach(function(element) {
+                    let div = $('<div></div>');
+                    let time = $('<div></div>').addClass('time').text(element.enter_time);
+                    let name = $('<div></div>').addClass('name').text(element.name);
+
+                    div.append(time).append(name);
+                    use_now.append(div);
+                });
+            }).fail(function() {
+                alert('通信に失敗しました')
+            })
+        }, 10000);
+    });
+})
